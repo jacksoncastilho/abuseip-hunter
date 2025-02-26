37a0f0f1981a1af6321e01951c350c99089dc9e1
jacksoncastilho <studyjacksoncastilho@gmail.com>
2025-02-26 12:18:17 -0500

    Separate the code into functions. Use a list of APIs in yaml

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # abuseip-hunter
+
+```
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install pyyaml
+```
+
+```
+deactivate
+```
--- main.py
+++ main.py
@@ -11,35 +11,49 @@
 import requests
 import argparse
 import sys
+import yaml
 
-URL = 'https://api.abuseipdb.com/api/v2/check'
-API_KEY = ''
+def getApi():
+    try:
+        f = open('./api.yaml', 'r')
+        api = yaml.safe_load(f)
+        f.close()
+        return api
+    except Exception as err:
+        print(err)
+
+def checkIpReputation(ip, api):
+    headers = {
+        'Accept': 'application/json',
+        'Key': api['key']
+    }
+
+    try:
+        resp = requests.get(api['url'], headers=headers, params={'ipAddress': ip})
+    except requests.exceptions.RequestException as err:
+        print(f"An exception occurred!\n{err}")
+
+    data = resp.json()
+    
+    if 'data' in data:
+        print(f"{data['data']['ipAddress']} was reported {data['data']['totalReports']} times. Confidence of Abuse is {data['data']['abuseConfidenceScore']}%")
+    else:
+        print(f"An exception occurred!\n{data}")
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-l', '--list', type=str, required=True, help='IP list')
+parser.add_argument('-ip', type=str, required=True, help='One IP or list of IPs')
 
 args = parser.parse_args()
 
-headers = {
-    'Accept': 'application/json',
-    'Key': API_KEY
-}
 try:
-    f = open(args.list, "r")
-    for ip in f:
-        try:
-            res = requests.get(URL, headers=headers, params={'ipAddress': ip})    
-            data = res.json()
-            if 'data' in data:
-                print(f"{data['data']['ipAddress']} was reported {data['data']['totalReports']} times. Confidence of Abuse is {data['data']['abuseConfidenceScore']}%")
-            else:
-                print(f"An exception occurred!\n{data}")
-                break
-        except Exception as err:
-            print(f"An exception occurred!\n{err}")
-            break
+    ips = open(args.ip, 'r')
 except Exception as err:
-    print(f"An exception occurred!\n{err}")
-f.close()
+    print(err)
 
+for ip in ips:
+    for api as getApi()['data']:
+       checkIpReputation(ip, api)
+
+ips.close()
+    
