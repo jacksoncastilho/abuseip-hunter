a9ec180e8b66f21eb8cab693b739eb3602864fac
jacksoncastilho <studyjacksoncastilho@gmail.com>
2025-02-27 13:37:27 -0500

    Switch between API keys

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ python3 -m venv venv
 
 source venv/bin/activate
 
+pip install requests
 pip install pyyaml
+pip install regex
 ```
 
 ```
--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-#! /usr/bin/python
-
-# - [x] pint message
-# - [x] read file
-# - [x] handle exceptions with try/catch
-# - [ ] handle end credits
-# - [ ] algumens
-# - [ ] add classes
-# - [ ] documentation
-
 import requests
 import argparse
 import sys
 import yaml
+import re
+from pathlib import Path
 
 def getApi():
     try:
         f = open('./api.yaml', 'r')
+
         api = yaml.safe_load(f)
+
         f.close()
+
         return api
     except Exception as err:
-        print(err)
+        print(f"An exception occurred!\n{err}")
 
 def checkIpReputation(ip, api):
     headers = {
@@ -32,29 +27,40 @@ def checkIpReputation(ip, api):
         resp = requests.get(api['url'], headers=headers, params={'ipAddress': ip})
     except requests.exceptions.RequestException as err:
         print(f"An exception occurred!\n{err}")
-
-    data = resp.json()
     
-    if 'data' in data:
+    if resp.status_code == 200:
+        data = resp.json()
         print(f"{data['data']['ipAddress']} was reported {data['data']['totalReports']} times. Confidence of Abuse is {data['data']['abuseConfidenceScore']}%")
     else:
-        print(f"An exception occurred!\n{data}")
+        print(f"An exception occurred!\nCheck your YAML file")
+        exit()
+
+def byFile():
+    try:
+        ips = open(args.ip, 'r')
+        
+        for ip in ips:
+            for apis in getApi()['apis']:
+                for api in apis.keys():
+                    checkIpReputation(ip, apis[api])
+
+        ips.close()
+    except Exception as err:
+        print(f"An exception occurred!\n{err}")
+
+def byIp():
+    for apis in getApi()['apis']:
+            for api in apis.keys():
+                checkIpReputation(args.ip, apis[api])
 
 parser = argparse.ArgumentParser()
-
-parser.add_argument('-ip', type=str, required=True, help='One IP or list of IPs')
+parser.add_argument('-ip', type=str, required=True, help='Enter a file or an IP')
 
 args = parser.parse_args()
 
-try:
-    ips = open(args.ip, 'r')
-except Exception as err:
-    print(err)
-
-for ip in ips:
-    for apis in getApi()['data']:
-        for api in apis.keys():
-            print(api)
-            #checkIpReputation(ip, api)
-ips.close()
-    
+if Path(args.ip).exists() and Path(args.ip).is_file():
+    byFile()
+elif re.fullmatch(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", args.ip):
+    byIp()
+else:
+    raise Exception(f"An exception occurred!\nThe IP or file does not exist")
